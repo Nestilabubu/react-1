6d12f27719cbb9ac26c61c178ff75ff81a3b32a3
Nestilabubu <mih80852@gmail.com>
2025-10-07 14:47:39 +0300

    useMemo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,12 +7,9 @@ import Logo from '../Logo/Logo';
 const logos = ['/logo.svg', '/vite.svg'];
 function Header() {
   const [logoIndex, setLogoIndex] = useState(0);
-  const [secondIndex, setSecondIndex] = useState(0);
 
 	const toggleLogo = () => {
 		setLogoIndex(state => Number(!state));
-		setSecondIndex(i => i + 1);
-		console.log(secondIndex);
 	};
   
 	return (
