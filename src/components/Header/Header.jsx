6e246111459612b7c877f722e3631da747468c45
Nestilabubu <mih80852@gmail.com>
2025-10-07 14:09:13 +0300

    context api

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,18 @@
+import SelectUser from '../SelectUser/SelectUser';
 import styles from './Header.module.css';
 
-function Header() {
-  <img className={styles.logo} src="/logo.svg" alt="Логотип журнала" />
+function Header({ changedUser }) {
+	const changeUser = (e) => {
+		changedUser(e.target.value);
+		console.log(e.target.value);
+	};
+	
+	return (
+		<>
+			<img className={styles.logo} src="/logo.svg" alt="Логотип журнала" />
+			<SelectUser changeUser={changeUser} />
+		</>
+	);
 }
 
 export default Header;
