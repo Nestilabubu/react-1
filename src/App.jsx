d2f2b1f41362d024d2ed0955b063cfec3d4640fc
Nestilabubu <mih80852@gmail.com>
2025-10-07 15:03:05 +0300

    удаление записей

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function mapItems(items) {
 function App() {
 	const [items, setItems] = useLocalStorage('data');
 	const [userId, setUserId] = useState(1);
-	const [selectedItem, setSelectedItem] = useState({});
+	const [selectedItem, setSelectedItem] = useState(null);
 
 	const addItem = item => {
 		if (!item.id) {
@@ -42,17 +42,21 @@ function App() {
 				return i;
 			})]);
 		}
+	}
 	};
+
+	const deleteItem = (id) => {
+	setItems([...items.filter(i => i.id !== id)]);
 
   return (
 	<UserContextProvidev value={{ userId, setUserId }}>
 		<div className='app'>
 			<LeftPanel>
 				<Header/>
-				<JournalAddButton/>
+				<JournalAddButton  clearForm={() => setSelectedItem(null)} />
 				<JournalList items={mapItems(items)} setItem={setSelectedItem}/>
 			</LeftPanel>
-			<Body><JournalForm onSubmit={addItem} data={selectedItem}/></Body>
+			<Body><JournalForm onSubmit={addItem}  onDelete={deleteItem} data={selectedItem}/></Body>
 		</div>
 	</UserContextProvidev>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,14 @@
 import SelectUser from '../SelectUser/SelectUser';
-import styles from './Header.module.css';
-import { useState } from 'react';
-import Button from '../Button/Button';
 import Logo from '../Logo/Logo';
 
 const logos = ['/logo.svg', '/vite.svg'];
-function Header() {
-  const [logoIndex, setLogoIndex] = useState(0);
 
-	const toggleLogo = () => {
-		setLogoIndex(state => Number(!state));
-	};
-  
+function Header() {
+
 	return (
 		<>
-			<Logo image={logos[logoIndex]} />
+			<Logo image={logos[0]} />
 			<SelectUser />
-      <Button onClick={toggleLogo}>Сменить лого</Button>
 		</>
 	);
 }
